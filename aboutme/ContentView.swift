86d90b890d60fff66edf834a108ecdbcc3ead59a
Gaelle <gaellee.valmir@gmail.com>
2023-07-14 10:16:38 -0400

    Initial Commit

---
+++ aboutme/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  aboutme
+//
+//  Created by Gaelle Valmir on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   
+    @State private var name = "I'm a Potter Head"
+    @State private var textTitle = ""
+    
+    var body: some View {
+        
+        
+        ZStack {
+            Color(.systemTeal)
+                .ignoresSafeArea()
+        VStack {
+            Spacer()
+            Image("ravenclaw1")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(15)
+                .padding()
+            
+            Spacer()
+            
+            Text("Hi, my name is Gaelle!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                .padding()
+
+            Spacer()
+            Text(textTitle)
+                .font(.title)
+                
+            Spacer()
+            Button("Reveal Fun fact") {
+                textTitle = name
+            }
+            .font(.title)
+            .buttonStyle(.borderedProminent)
+            .tint(.blue)
+            Spacer()
+            
+            
+            }
+        }
+
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ aboutme/aboutmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutmeApp.swift
+//  aboutme
+//
+//  Created by Gaelle Valmir on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct aboutmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
